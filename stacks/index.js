dc66f2b610a298706443ca1fd7fb32dd20c7a560
Jogalaxmi rath <106918937+Jogalaxmi2002@users.noreply.github.com>
2022-08-07 11:41:03 +0530

    Add files via upload

---
+++ stacks/index.js
@@ -0,0 +1,17 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+import { FrontendStack } from "./FrontendStack";
+import { AuthStack } from "./AuthStack";
+import { ApiStack } from "./ApiStack";
+import { StorageStack } from "./StorageStack";
+
+
+export default function main(app) {
+  app.setDefaultFunctionProps({
+    runtime: "nodejs16.x",
+    srcPath: "services",
+    bundle: {
+      format: "esm",
+    },
+  });
+  app.stack(StorageStack).stack(ApiStack).stack(AuthStack).stack(FrontendStack);
+}
